755d2286f0d2ce37b58027ab3e3857d627d2de96
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-11-26 04:09:40 +0600

    Product Add Form Completed

--- src/Components/Dashboards/AddAProduct/AddAProduct.js
+++ src/Components/Dashboards/AddAProduct/AddAProduct.js
@@ -1,9 +1,189 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddAProduct = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const handleAddProduct = (data, e) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <p>This is Add A Product Page</p>
+    <div className="p-10">
+      <form onSubmit={handleSubmit(handleAddProduct)}>
+        <div className="flex justify-around">
+          <div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Product Name: </span>
+                <input
+                  type="text"
+                  {...register("ProductName", {
+                    required: "Name is Required",
+                  })}
+                  className="input input-bordered justify-end max-w-xs"
+                />
+                {errors.name && (
+                  <p className="text-red-500">{errors.name.message}</p>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text pr-5">Selling Price: </span>
+                <input
+                  type="text"
+                  {...register("SellingPrice", {
+                    required: "Name is Required",
+                  })}
+                  className="input input-bordered justify-end max-w-xs"
+                />
+                {errors.name && (
+                  <p className="text-red-500">{errors.name.message}</p>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Mobile Number: </span>
+                <input
+                  type="text"
+                  {...register("MobileNumber", {
+                    required: "Name is Required",
+                  })}
+                  className="input input-bordered justify-end max-w-xs"
+                />
+                {errors.name && (
+                  <p className="text-red-500">{errors.name.message}</p>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Condition-Type: </span>
+                <select
+                  className="select select-bordered max-w-xs"
+                  defaultValue={"Select Conditions"}
+                  {...register("ProductCondition", {})}
+                >
+                  <option disabled defaultValue={"Select Conditions"}>
+                    Select Conditions
+                  </option>
+                  <option>Excellent</option>
+                  <option>Good</option>
+                  <option>Fair</option>
+                </select>
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Upload-Image: </span>
+                <input
+                  type="file"
+                  {...register("Image", {
+                    required: "Image is Required",
+                  })}
+                  className="file-input file-input-bordered file-input-ghost justify-end w-28"
+                />
+              </label>
+            </div>
+          </div>
+          <div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Location: </span>
+                <input
+                  type="text"
+                  {...register("Location", {
+                    required: "Name is Required",
+                  })}
+                  className="input input-bordered justify-end max-w-xs"
+                />
+                {errors.name && (
+                  <p className="text-red-500">{errors.name.message}</p>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Description: </span>
+                <input
+                  type="text"
+                  {...register("ProductDescription", {
+                    required: "Name is Required",
+                  })}
+                  className="input input-bordered justify-end max-w-xs"
+                />
+                {errors.name && (
+                  <p className="text-red-500">{errors.name.message}</p>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Original Price: </span>
+                <input
+                  type="text"
+                  {...register("OriginalPrice", {
+                    required: "Name is Required",
+                  })}
+                  className="input input-bordered justify-end max-w-xs"
+                />
+                {errors.name && (
+                  <p className="text-red-500">{errors.name.message}</p>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Product Category: </span>
+                <select
+                  className="select select-bordered  max-w-xs"
+                  defaultValue={"Select Category"}
+                  {...register("ProductCategory", {})}
+                >
+                  <option disabled defaultValue={"Select Category"}></option>
+                  <option>apple</option>
+                  <option>hp</option>
+                  <option>others</option>
+                </select>
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Year Of Purchase: </span>
+                <input
+                  type="text"
+                  {...register("name", {
+                    required: "Name is Required",
+                  })}
+                  className="input input-bordered justify-end max-w-xs"
+                />
+                {errors.name && (
+                  <p className="text-red-500">{errors.name.message}</p>
+                )}
+              </label>
+            </div>
+          </div>
+        </div>
+        <input
+          className="btn btn-primary w-full max-w-xs mt-4 flex mx-auto"
+          value="Add Product"
+          type="submit"
+        />
+      </form>
     </div>
   );
 };
--- src/Components/Routes/AdminRoute.js
+++ src/Components/Routes/AdminRoute.js
@@ -13,7 +13,7 @@ const AdminRoute = ({ children }) => {
       <div role="status">
         <svg
           aria-hidden="true"
-          class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -27,7 +27,7 @@ const AdminRoute = ({ children }) => {
             fill="currentFill"
           />
         </svg>
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
       </div>
     );
   }
